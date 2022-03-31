1cfc8360c8a485671d7fb9dd656661c8b9642f89
HarshdeepKaurDhunna <97932988+HarshdeepKaurDhunna@users.noreply.github.com>
2022-03-31 02:03:24 -0400

    Done Archiving using UserDefault

--- Inventory/DetailsViewController.swift
+++ Inventory/DetailsViewController.swift
@@ -9,6 +9,11 @@ import UIKit
 
 class DetailsViewController: UIViewController {
 
+    var itemInventory: ItemList!
+    var selectedItem: Int?
+    
+    var userVal: UserDefaults!
+
     @IBOutlet weak var nameField: UITextField!
     @IBOutlet weak var SKUField: UITextField!
     @IBOutlet weak var descField: UITextField!
@@ -17,11 +22,33 @@ class DetailsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+         //Set Data to items and validate
+        if(nil != selectedItem){
+            let inventoryVal = itemInventory.items[selectedItem!]
+            nameField.text = inventoryVal.name
+            SKUField.text = inventoryVal.SKU
+            descField.text = inventoryVal.desc
+            dateField.date = inventoryVal.dateAdded
+        }
         
     }
     
+    //This method is to save or update data
     @IBAction func save(_ sender: Any) {
+    let val = Item(name: nameField.text!,SKU: SKUField.text!,description: descField.text!, dateAdded: dateField.date)
+    if(nil != index) {
+        itemInventory.items.remove(at: index!)
+        itemInventory.items.insert(val, at: index!)
+
+    }else {
+        itemInventory.addItem(item: val)
+    }
+        do {
+            
+            let itemEncodedVal = try NSKeyedArchiver.archivedData(withRootObject: itemInventory.items, requiringSecureCoding:false)
+            userVal.set(itemEncodedVal, forKey: "inventory")
+        } catch  {}
+        
     }
     
     /*
--- Inventory/ItemTableViewController.swift
+++ Inventory/ItemTableViewController.swift
@@ -9,6 +9,11 @@ import UIKit
 
 class ItemTableViewController: UITableViewController {
 
+    let itemInventory = ItemList()
+    var selectedItem:Int?
+    
+    var userVal = UserDefaults()
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -17,6 +22,14 @@ class ItemTableViewController: UITableViewController {
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
          self.navigationItem.leftBarButtonItem = self.editButtonItem
+
+        do {
+            if let itemVals = userVal.object(forKey: "inventory") {
+                let itemDecodedVal = try NSKeyedUnarchiver.unarchiveTopLevelObjectWithData(data as! Data) as? [Item]
+                itemInventory.items = decodedItems!
+            }
+        } catch  {}
+        
     }
 
     // MARK: - Table view data source
@@ -28,15 +41,18 @@ class ItemTableViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 1
+        return itemInventory.items.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "item", for: indexPath)
 
-        // Configure the cell...
+        //Show the name of item
+        let itemName = inventory.items[indexPath.row]
+        cell.textLabel?.text = itemName.name
 
         return cell
+
     }
 
     /*
@@ -47,24 +63,27 @@ class ItemTableViewController: UITableViewController {
     }
     */
 
-    /*
+    
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            // Delete the row from the data source
+            // Row delete from source
+            itemInventory.deleteItem(row: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
+            userDefaultsUpdate()
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
-    */
+    
 
-    /*
+    
     // Override to support rearranging the table view.
     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
+        itemInventory.moveItem(from: fromIndexPath.row, to: to.row)
+        userDefaultsUpdate()
     }
-    */
+    
 
     /*
     // Override to support conditional rearranging of the table view.
@@ -78,9 +97,28 @@ class ItemTableViewController: UITableViewController {
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        let outputVal = segue.destination as! DetailsViewController
+        
+        outputVal.inventory = itemInventory;
+        outputVal.userVal =  userDefaults;
+        
+        if(segue.identifier == "edit"){
+            let rowVal: UITableViewCell = sender as! UITableViewCell
+            outputVal.index = tableView.indexPath(for: rowVal)?.row
+        }
         
     }
 
+     override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        tableView.reloadData()
+    }
+
+    func userDefaultsUpdate{
+        do {
+            
+            let itemEncodedVal = try NSKeyedArchiver.archivedData(withRootObject: inventory.items, requiringSecureCoding:false)
+            userVal.set(itemEncodedVal, forKey: "inventory")
+        } catch  {}
+    }
 }
